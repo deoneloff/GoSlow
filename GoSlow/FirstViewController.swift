75d793ca14377ce74b8fcaacd20f0a035de66886
Deon Eloff <deoneloff@gmail.com>
2017-10-29 20:57:18 +0100

    Ongoing

--- GoSlow/FirstViewController.swift
+++ GoSlow/FirstViewController.swift
@@ -8,16 +8,27 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, CLLocationManagerDelegate {
     
+    let locationManager = CLLocationManager()
     let regionRadius: CLLocationDistance = 2000
     let almerePoort = [52.339200,5.142912]
-    
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        isAuthorizedtoGetUserLocation()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        }
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestLocation();
+        }
         let initialLocation = CLLocation(latitude: almerePoort[0], longitude: almerePoort[1])
         centerMapOnLocation(location: initialLocation)
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,5 +46,32 @@ class FirstViewController: UIViewController {
         mapView.setRegion(coordinateRegion, animated: true)
     }
     
+    //if we have no permission to access user location, then ask user for permission.
+    func isAuthorizedtoGetUserLocation() {
+        
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse     {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
     
+    
+    //this method will be called each time when a user change his location access preference.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            print("User allowed us to access location")
+            //do whatever init activities here.
+        }
+    }
+    
+    
+    //this method is called by the framework on         locationManager.requestLocation();
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Did location updates is called")
+        //store the user location here to firebase or somewhere
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Did location updates is called but failed getting location \(error)")
+    }
+
 }
